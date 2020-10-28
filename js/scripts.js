b9734826dc2d2c413f62de6e01dabb3821f30cd4
Nick Gottschlich <nicholas.gottschlich@procore.com>
2020-10-27 22:53:08 -0500

    intersection observer working

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+function handler(entries) {
+  for (entry of entries) {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+      console.log('we see it')
+    } else {
+      console.log('we dont see it')
+    }
+  }
+}
+
+let observer = new IntersectionObserver(handler);
+console.log('hello', document.getElementById('firstTarget'))
+observer.observe(document.getElementById('firstTarget'));
